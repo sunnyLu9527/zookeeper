85d2f23ebb1fc94689cc3e8fc50bad05b79e2732
renyong <497269678@qq.com>
2019-03-10 14:45:37 +0800

    分布式锁和分布式配置中心

--- src/java/vip/com/luban/distributed_config/Config.java
+++ src/java/vip/com/luban/distributed_config/Config.java
@@ -2,7 +2,6 @@ package com.luban.distributed_config;
 
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.framework.recipes.cache.ChildData;
 import org.apache.curator.framework.recipes.cache.PathChildrenCache;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheEvent;
 import org.apache.curator.framework.recipes.cache.PathChildrenCacheListener;
@@ -11,9 +10,8 @@ import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.data.Stat;
 
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * *************书山有路勤为径***************
@@ -24,76 +22,52 @@ import java.util.Set;
  * *************学海无涯苦作舟***************
  */
 public class Config {
+    private static final String CONFIG_PREFIX = "/CONFIG";
 
     private CuratorFramework client;
-    private static final String DEFAULT_BASEPATH = "/distributed_config";
-    private static final String DEFAULT_NAMES_PATH = "/distributed_config_names";
-    private Map<String, String> cacheMap = new HashMap<String, String>();
+    private Map<String, String> cache = new HashMap<>();
 
-
-    public Config(String connectStr) {
-        client = CuratorFrameworkFactory.newClient(connectStr, new RetryNTimes(Integer.MAX_VALUE, 1000))
-                .usingNamespace(DEFAULT_BASEPATH);
+    public Config(String address) {
+        client = CuratorFrameworkFactory.newClient(address,
+                new RetryNTimes(3, 1000));
+        ;
         client.start();
+
+        init();
     }
 
-    private void init() {
-        _initWatch();
-        _init();
-    }
-
-    /**
-     * 配置中心启动时需要把所有的配置项拿到本地来
-     */
-    private void _init() {
-        Set<String> names = _getNames();
-        for (String name : names) {
-            cacheMap.put(name, getDataFromZk(name));
-        }
-    }
-
-    private String getDataFromZk(String name) {
+    public void init() {
         try {
-            return new String(client.getData().forPath(name));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+            List<String> childrenNames = client.getChildren().forPath(CONFIG_PREFIX);
+            for (String name : childrenNames) {
+                String value = new String(client.getData().forPath(getConfigFullName(name)));
+                cache.put(name, value);
+            }
 
-        return null;
-    }
+            // 监听
+            PathChildrenCache watcher = new PathChildrenCache(client, CONFIG_PREFIX, true);
 
-    private void _initWatch() {
-        PathChildrenCache watcher = new PathChildrenCache(client, DEFAULT_BASEPATH, true);
+            watcher.getListenable().addListener(new PathChildrenCacheListener() {
+                @Override
+                public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
+                    String path = event.getData().getPath();
+                    System.out.println(event);
 
-        watcher.getListenable().addListener(new PathChildrenCacheListener() {
-            @Override
-            public void childEvent(CuratorFramework client, PathChildrenCacheEvent event) throws Exception {
-                ChildData data = event.getData();
-                if (data == null) {
-                    System.out.println("No data in event, event = " + event);
-                } else {
-                    System.out.println("Receive event: "
-                            + "type=[" + event.getType() + "]"
-                            + ", path=[" + data.getPath() + "]"
-                            + ", data=[" + new String(data.getData()) + "]"
-                            + ", stat=[" + data.getStat() + "]");
+                    if (path.startsWith(CONFIG_PREFIX)) {
+                        String key = path.replace(CONFIG_PREFIX + "/", "");
+                        if (PathChildrenCacheEvent.Type.CHILD_ADDED.equals(event.getType()) ||
+                                PathChildrenCacheEvent.Type.CHILD_UPDATED.equals(event.getType())) {
+                            String value = new String(event.getData().getData());
+                            cache.put(key, value);
 
-                    if (event.getType() == PathChildrenCacheEvent.Type.CHILD_ADDED
-                            || event.getType() == PathChildrenCacheEvent.Type.CHILD_UPDATED) {
-                        String path = data.getPath();
-                        if (path.startsWith(DEFAULT_BASEPATH)) {
-                            String key = path.replace(DEFAULT_BASEPATH + "/", "");
-                            String dataStr = new String(data.getData());
-                            cacheMap.put(key, dataStr);
+                        } else if (PathChildrenCacheEvent.Type.CHILD_REMOVED.equals(event.getType())) {
+                            cache.remove(key);
                         }
                     }
                 }
+            });
 
-            }
-        });
-
-        try {
-            watcher.start(PathChildrenCache.StartMode.BUILD_INITIAL_CACHE);
+            watcher.start();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -101,68 +75,32 @@ public class Config {
     }
 
 
-    public void save(String key, String value) {
-
-        // 存key, value
-        cacheMap.put(key, value);
-        _save(decKey(key), value.getBytes());
-
-        // 存names
-        Set<String> names = _getNames();
-        names.add(decKey(key));
-        _save(DEFAULT_NAMES_PATH, SerializableUtils.encode(names));
-
-    }
-
-    public String get(String key) {
-        return cacheMap.get(key);
-    }
-
-
-    /**
-     * 作为配置，一个基本的格式就是{key:value}
-     *
-     * @param key
-     * @param value
-     */
-    private void _save(String key, byte[] value) {
+    // 新增或更新配置，一个配置项对应一个zookeeper节点，节点内容为配置项值
+    public void save(String name, String value) {
         try {
-            // 当前key是否已存在，如果存在则更新数据，不存在则更新
-            Stat stat = client.checkExists().forPath(key);
+            String configFullName = getConfigFullName(name);
+            Stat stat = client.checkExists().forPath(configFullName);
             if (stat != null) {
-                client.setData().forPath(key, value);
+                // update
+                client.setData().forPath(configFullName, value.getBytes());
             } else {
-                client.create()
-                        .creatingParentsIfNeeded()
-                        .withMode(CreateMode.PERSISTENT)
-                        .forPath(key, value);
+                // create
+                client.create().creatingParentsIfNeeded().withMode(CreateMode.PERSISTENT).forPath(configFullName, value.getBytes());
             }
+
+            cache.put(name, value);
+
         } catch (Exception e) {
             e.printStackTrace();
         }
 
     }
 
-
-    private Set<String> _getNames() {
-        try {
-            Stat stat = client.checkExists().forPath(DEFAULT_NAMES_PATH);
-            if (stat != null) {
-                byte[] namesData = client.getData().forPath(DEFAULT_NAMES_PATH);
-
-                if (namesData != null && namesData.length > 0) {
-                    Set<String> names = (Set<String>) SerializableUtils.decode(namesData);
-                    return names;
-                }
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return new HashSet<String>();
+    private String getConfigFullName(String name) {
+        return CONFIG_PREFIX + "/" + name;
     }
 
-    private String decKey(String key) {
-        return DEFAULT_BASEPATH + "/" + key;
+    public String get(String name) {
+        return cache.get(name);
     }
 }
